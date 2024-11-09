34556411ab5ad881c9a8f4d2178b49b876514556
ibryam <ibryamfibryam@gmail.com>
2024-11-09 11:10:52 +0200

    added high score and data file to save all time records

--- Projects/Intermediate/snake_game/scoreboard.py
+++ Projects/Intermediate/snake_game/scoreboard.py
@@ -1,30 +1,36 @@
 from turtle import Turtle
 ALIGNMENT = "center"
-FONT = ("Arial", 12, "normal")
+FONT = ("Arial", 18, "normal")
 
 
 class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
-
         self.score = 0
-        self.penup()
+        with open("data.txt") as d:
+            self.high_score = int(d.read())
         self.color("white")
-        self.goto(0, 280)
+        self.penup()
+        self.goto(0, 270)
         self.hideturtle()
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align= ALIGNMENT, font= FONT)
-
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER.",align= ALIGNMENT, font= FONT)
-
-    def increase_score(self):
-        self.score += 15
         self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
+
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode = "w") as d:
+                high_score = d.write(f"{self.high_score}")
+
+        self.score = 0
         self.update_scoreboard()
 
 
+    def increase_score(self):
+        self.score += 15
+        self.update_scoreboard()
